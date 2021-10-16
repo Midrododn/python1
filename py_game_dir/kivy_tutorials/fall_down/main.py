ada92b0a8929d87f759416b4dd796cb1db31b602
lock <vitalijs.bekasovs@gmail.com>
2021-10-16 11:12:31 +0200

    pre compile

--- py_game_dir/kivy_tutorials/fall_down/main.py
+++ py_game_dir/kivy_tutorials/fall_down/main.py
@@ -5,6 +5,9 @@ from kivy.properties import (
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
+
+from kivy.core.window import Window
+
 from random import randint
 
 
@@ -13,10 +16,19 @@ class PongBall(Widget):
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
     acc = 1
+    air_friction = 0.1
 
     def move(self, height, width):
         self.velocity_y -= self.acc
-        self.pos = Vector(*self.velocity) + self.pos
+        self.y += self.velocity_y
+        if (self.velocity_x < 0):
+            self.velocity_x += self.air_friction
+        elif (self.velocity_x > 0):
+            self.velocity_x -= self.air_friction
+        if abs(self.velocity_x) < 0.5:
+            self.velocity_x = 0
+        self.x += self.velocity_x
+        #self.pos = Vector(*self.velocity) + self.pos
         # bounce off bottom
         if (self.y <= 10):
             if (self.velocity_y <= -3):
@@ -34,8 +46,12 @@ class PongBall(Widget):
             self.acc = 0
 
         # bounce off left and right
-        if (self.x < 0) or (self.right > width):
+        if (self.x < 0):
+            self.velocity_x *= -1
+            self.x = 0
+        if (self.x > width - 20):
             self.velocity_x *= -1 
+            self.x = width -20
 
 
 class PongGame(Widget):
@@ -51,9 +67,13 @@ class PongGame(Widget):
     def on_touch_down(self, touch):
         if touch.y >= self.height - 50:
             if self.ball.velocity_y == 0:
-                self.ball.velocity_y = 20
-                self.ball.acc = 1    
-
+                self.ball.velocity_y = 30
+                self.ball.acc = 1
+        if ((touch.y < self.height - 50) and (touch.y >= self.height -150)):
+            if (touch.x <= 100):
+                self.ball.velocity_x = -20
+            if (touch.x >= self.width - 100):
+                self.ball.velocity_x = 20
 
 class FallApp(App):
     def build(self):
@@ -64,4 +84,5 @@ class FallApp(App):
 
 
 if __name__ == '__main__':
+    #Window.fullscreen = True  or 'auto'
     FallApp().run()
