c0e609ff58fa37df7de2b4c8e449a7cd1db9a8e9
lock <vitalijs.bekasovs@gmail.com>
2021-10-18 22:18:32 +0200

    - assdir

--- py_game_dir/assembly_dir/.buildozer/android/platform/build-armeabi-v7a/dists/myapp__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/main/__/__/SDL/src/main/android/SDL_android_main.o.d
+++
@@ -1,2 +0,0 @@
-/home/lock/Documents/pydir/py_game_dir/assembly_dir/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/main/__/__/SDL/src/main/android/SDL_android_main.o: \
-  /home/lock/Documents/pydir/py_game_dir/assembly_dir/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/application/src/../../SDL/src/main/android/SDL_android_main.c
--- py_game_dir/assembly_dir/.buildozer/android/platform/build-armeabi-v7a/dists/pytxtcheck__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/lock/Documents/pydir/py_game_dir/assembly_dir/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/lock/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.c \
-  /home/lock/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h
-
-/home/lock/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h:
